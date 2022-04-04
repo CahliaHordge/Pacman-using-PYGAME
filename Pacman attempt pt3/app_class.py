6e7e89102808cbc1893cb026df5723e5c953f2bb
CahliaHordge <98338922+CahliaHordge@users.noreply.github.com>
2022-04-04 15:42:39 -0500

    Add files via upload

---
+++ Pacman attempt pt3/app_class.py
@@ -0,0 +1,42 @@
+import pygame
+from settings import *
+import sys
+
+
+pygame.init()
+vec = pygame.math.Vector2
+
+
+class App:
+    def __init__(self) -> None:
+        self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.state = 'start'
+
+    def run(self):
+        while self.running:
+            if self.state == 'start':
+                self.intro_events()
+                self.intro_update()
+                self.intro_draw
+            else:
+                pass
+            self.clock.tick(FPS)
+        pygame.quit()
+        sys.exit()
+
+######################## INTRO FUNCTIONS ################################
+
+    def intro_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.state = 'playing'
+    def start_update(self):
+        pass
+
+    def start_draw(self):
+        self.screen.fill(BLACK)
+        pygame.display.update()
---
+++ Pacman attempt pt3/settings.py
@@ -0,0 +1,9 @@
+#Screen Setttings 
+WIDTH, HEIGHT = 448, 596
+FPS = 60
+
+
+#Color settings
+BLACK = (0,0,0)
+#player setting
+#enemy settings
