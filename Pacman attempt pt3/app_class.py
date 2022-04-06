c6916ecb77894e9204e17fa1cdcbd56e85ae843d
CahliaHordge <98338922+CahliaHordge@users.noreply.github.com>
2022-04-06 09:08:45 -0500

    Update app_class.py

--- Pacman attempt pt3/app_class.py
+++ Pacman attempt pt3/app_class.py
@@ -3,8 +3,12 @@ from settings import *
 import sys
 
 
+
+
 pygame.init()
 vec = pygame.math.Vector2
+
+
 
 
 class App:
@@ -14,19 +18,31 @@ class App:
         self.running = True
         self.state = 'start'
 
+
     def run(self):
         while self.running:
             if self.state == 'start':
                 self.intro_events()
-                self.intro_update()
-                self.intro_draw
-            else:
-                pass
+                self.start_update()
+                self.start_draw()
+            elif self.state == 'playing':
+                 self.playing_events()
+                 self.playing_update()
+                 self.playing_draw()
             self.clock.tick(FPS)
         pygame.quit()
         sys.exit()
-######################## Help Functions ################################
 
+
+######################## Help Functions ################################
+    def draw_text(self, words, screen, pos, size, color, font_name, centered=False):
+        font = pygame.font.SysFont(font_name, size)
+        text = font.render(words, False, color)
+        text_sixe = text.get_size()
+        if centered:
+            pos[0] = pos[0]-text_sixe[0]//2
+            pos[1] = pos[1]-text_sixe[1]//2
+        screen.blit(text, pos)
 
 
 ######################## INTRO FUNCTIONS ################################
@@ -40,7 +56,27 @@ class App:
     def start_update(self):
         pass
 
+
     def start_draw(self):
         self.screen.fill(BLACK)
-        self.draw_text(self.screen,START_TEXT_SIZE,(170, 132,50))
+        self.draw_text('PUSH SPACE BAR',self.screen,[WIDTH//2, HEIGHT//2-50],
+        START_TEXT_SIZE,(170, 132,50), START_FONT, centered=True)
+        self.draw_text('1 PLAYER ONLY',self.screen,[WIDTH//2, HEIGHT//2+50],
+        START_TEXT_SIZE,(44, 167, 198), START_FONT, centered=True)
+        self.draw_text('HIGH SCORE',self.screen,[4,0],
+        START_TEXT_SIZE,(255,255,255), START_FONT)
+        pygame.display.update()
+
+######################## Playing FUNCTIONS ################################
+
+    def playing_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+    def playing_update(self):
+        pass
+
+
+    def playing_draw(self):
+        self.screen.fill(RED)
         pygame.display.update()
