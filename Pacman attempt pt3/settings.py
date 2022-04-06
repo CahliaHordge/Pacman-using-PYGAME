bf05b1ba44c2bdbf442c23e461d3c5bee05d973f
CahliaHordge <98338922+CahliaHordge@users.noreply.github.com>
2022-04-06 09:09:14 -0500

    Update settings.py

--- Pacman attempt pt3/settings.py
+++ Pacman attempt pt3/settings.py
@@ -5,10 +5,11 @@ FPS = 60
 
 #Color settings
 BLACK = (0,0,0)
+RED = (208, 22, 22)
 
 # Font settings
-
 START_TEXT_SIZE = 16
-START_FONT - "arial black."
+START_FONT = "arial black"
+
 #player setting
 #enemy settings
