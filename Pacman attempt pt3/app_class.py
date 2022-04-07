6a069eff53e2c43f106a882ea30c29af7e88571c
CahliaHordge <98338922+CahliaHordge@users.noreply.github.com>
2022-04-07 12:53:51 -0500

    Update app_class.py

--- Pacman attempt pt3/app_class.py
+++ Pacman attempt pt3/app_class.py
@@ -17,25 +17,31 @@ class App:
         self.clock = pygame.time.Clock()
         self.running = True
         self.state = 'start'
+        self.cell_width = MAZE_WIDTH//28
+        self.cell_height = MAZE_HEIGHT//30
+        
+        self.load()
 
-
+        
     def run(self):
         while self.running:
             if self.state == 'start':
-                self.intro_events()
+                self.start_events()
                 self.start_update()
                 self.start_draw()
             elif self.state == 'playing':
                  self.playing_events()
                  self.playing_update()
                  self.playing_draw()
-            self.clock.tick(FPS)
+            else:
+                self.clock.tick(FPS)
         pygame.quit()
         sys.exit()
 
 
 ######################## Help Functions ################################
-    def draw_text(self, words, screen, pos, size, color, font_name, centered=False):
+    def draw_text(self, words, screen, pos, size,
+     color, font_name, centered=False):
         font = pygame.font.SysFont(font_name, size)
         text = font.render(words, False, color)
         text_sixe = text.get_size()
@@ -43,16 +49,27 @@ class App:
             pos[0] = pos[0]-text_sixe[0]//2
             pos[1] = pos[1]-text_sixe[1]//2
         screen.blit(text, pos)
-
-
+    
+    def load(self): 
+        self.background = pygame.image.load('maze.png')
+        self.background = pygame.transform.scale(self.background,
+        (MAZE_WIDTH, MAZE_HEIGHT))
+    
+    def draw_grid(self):
+            for x in range (WIDTH//self.cell_width):
+                pygame.draw.line(self.background, GREY, (x*self.cell_width, 0),
+                 (x*self.cell_width, HEIGHT))
+            for x in range (HEIGHT//self.cell_height):
+                pygame.draw.line(self.background, GREY,(0,x*self.cell_height),(WIDTH, x*self.cell_height))
 ######################## INTRO FUNCTIONS ################################
 
-    def intro_events(self):
+    def start_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 self.state = 'playing'
+    
     def start_update(self):
         pass
 
@@ -78,5 +95,10 @@ class App:
 
 
     def playing_draw(self):
-        self.screen.fill(RED)
+        self.screen.blit(self.background,(TOP_BOTTOM_BUFFER//2, 
+        TOP_BOTTOM_BUFFER//2))
+        self.draw_grid()
+        self.draw_text(self, 'HIGH SCORE: 0', self.screen, (10,5), 18, WHITE, 
+        START_FONT)
         pygame.display.update()
+        
